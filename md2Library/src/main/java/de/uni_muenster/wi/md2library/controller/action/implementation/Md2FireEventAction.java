f0a375f8867cd96c56e14666685898ed519508af
Christoph Rieger <rieger.chris@googlemail.com>
2016-08-23 14:31:53 +0200

    Continue workflow classes and handlers

--- md2Library/src/main/java/de/uni_muenster/wi/md2library/controller/action/implementation/Md2FireEventAction.java
+++ md2Library/src/main/java/de/uni_muenster/wi/md2library/controller/action/implementation/Md2FireEventAction.java
@@ -1,26 +1,38 @@
 package de.uni_muenster.wi.md2library.controller.action.implementation;
 
+import de.uni_muenster.wi.md2library.workflow.Md2WorkflowAction;
+import de.uni_muenster.wi.md2library.workflow.Md2WorkflowElement;
+import de.uni_muenster.wi.md2library.workflow.Md2WorkflowEventRegistry;
+import de.uni_muenster.wi.md2library.workflow.Md2WorkflowManager;
+
 /**
  * Used to throw a workflow event.
  * Represents FireEventAction in MD2-DSL
- * TODO: implement Md2FireEventAction
  * <p/>
- * Created on 10/08/2015
+ * Created on 23/08/2016
  *
- * @author Fabian Wrede
+ * @author Christoph Rieger
  * @version 1.0
  * @since 1.0
  */
 public class Md2FireEventAction extends AbstractMd2Action {
+
     /**
      * Instantiates a new Md 2 fire event action.
      */
-    public Md2FireEventAction() {
-        super("Md2FireEventAction");
+    public Md2FireEventAction(String name) {
+        super("Md2FireEventAction" + name);
     }
 
     @Override
     public void execute() {
-        // TODO: Implement Md2FireEventAction
+        Md2WorkflowElement workflowElement = Md2WorkflowEventRegistry.getInstance().getWorkflowEventTarget(this.actionSignature);
+        Md2WorkflowAction workflowAction = Md2WorkflowEventRegistry.getInstance().getWorkflowEventAction(this.actionSignature);
+
+        if (workflowAction.equals(Md2WorkflowAction.START)) {
+            Md2WorkflowManager.getInstance().goToWorkflow(workflowElement);
+        } else if (workflowAction.equals(Md2WorkflowAction.END)) {
+            Md2WorkflowManager.getInstance().endWorkflow(workflowElement);
+        }
     }
 }
--- md2Library/src/main/java/de/uni_muenster/wi/md2library/workflow/Md2WorkflowAction.java
+++ md2Library/src/main/java/de/uni_muenster/wi/md2library/workflow/Md2WorkflowAction.java
@@ -4,5 +4,5 @@ package de.uni_muenster.wi.md2library.workflow;
  * Created by c_rieg01 on 23.08.2016.
  */
 public enum Md2WorkflowAction {
-    Start, End;
+    START, END;
 }
