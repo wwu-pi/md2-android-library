6fed766df7d89be182cc152104445ca047a93cd1
i7-3770k <>
2017-06-12 21:08:01 +0200

    SensorHelper erweitert

--- md2Library/src/main/java/de/uni_muenster/wi/md2library/SensorHelper.java
+++ md2Library/src/main/java/de/uni_muenster/wi/md2library/SensorHelper.java
@@ -1,4 +1,4 @@
-package de.uni_muenster.wi.md2library.model;
+package md2.pswa_demo;
 
 import android.content.Context;
 import android.hardware.Sensor;
@@ -59,6 +59,64 @@ public class SensorHelper {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 setSensorValue(event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        //Prüfen ob SensorManager vorhanden
+        if(mSensorManager!=null){
+            //Prüfen ob der entsprechende Sensor überhaupt verfügbar ist sieh Initialisierung
+            if(mSensor != null){
+                mSensorManager.registerListener(_SensorEventListener, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
+                //Debug
+                System.out.println("Sensor:" + this.sensorType + " gefunden:");
+            }
+            else{
+                System.out.println("Sensor:" + this.sensorType + " nicht gefunden:");
+                setSensorValue(Float.parseFloat("-99999.99"));
+            }
+        }
+    }
+
+    // /Konstruktor damit getSystemService funktioniert, da diese Klasse keine Activity ist muss der Context übergeben werden
+    public SensorHelper(Context mContext, String _varName, String _sensorType, final String Axis) {
+        this.mContext = mContext;
+        this.varName = _varName;
+        this.sensorType = _sensorType;
+
+        mSensorManager = (SensorManager) mContext.getSystemService(mContext.SENSOR_SERVICE);
+
+        //Swicht Case nicht problemlos anwendbar in der verwendeten JavaVersion
+        //Debug Syso
+        System.out.println("IF");
+        if(sensorType.equals("accelerometer")){
+            System.out.println("Switch accelerometer");
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+        if(sensorType.equals("gyroskop")){
+            System.out.println("Switch gyroskop");
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        }
+        //Eventlistener für den Sensor
+        SensorEventListener _SensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if(Axis.equals("X")){
+                    setSensorValue(event.values[0]);
+                    System.out.println("Achse: X");
+                }
+                if(Axis.equals("Y")){
+                    setSensorValue(event.values[1]);
+                    System.out.println("Achse: Y");
+                }
+                if(Axis.equals("Z")){
+                    setSensorValue(event.values[2]);
+                    System.out.println("Achse: Z");
+                }
             }
 
             @Override
